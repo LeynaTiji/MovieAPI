48bc1076f20b73b963de1775afa616fc8f6d4746
LeynaTiji <leynatiji@icloud.com>
2026-03-18 11:57:56 +0000

    changed semantic function to iterate through reviews

--- app/hf_semantic_analysis.py
+++ app/hf_semantic_analysis.py
@@ -3,19 +3,23 @@ from transformers import pipeline
 # code created referencing https://huggingface.co/blog/sentiment-analysis-python
 
 # load bart model
-summeriser = pipeline("text-generation", model="gpt2")
+
 sentiment_model = pipeline("sentiment-analysis", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
 
 def review_semantics(reviews: list[str]):
 
-    #join all reviews together
-    joint_review = " ".join(reviews)
+    sentiment = []
 
     # code created referencing https://huggingface.co/blog/sentiment-analysis-python
-    # sentiment analysis
-    sentiment = sentiment_model(joint_review)[0]
-    label = sentiment["label"]
-    score = sentiment["score"]
+    for r in reviews:
+        # sentiment analysis
+        result = sentiment_model(r)[0]
+        # negate score if sentiment was not positive
+        sentiment.append(result["score"] if result["label"] == "POSITIVE" else -result["score"])
 
-    return label, score
+    average_score = sum(sentiment) / len(sentiment)
+
+    label = "POSITIVE" if average_score > 0 else "NEGATIVE"
+
+    return label, abs(average_score)
 
