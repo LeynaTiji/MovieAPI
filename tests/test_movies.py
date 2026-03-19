2a8e020b699a0bd0b1d7983b13d0610f63f6fed4
LeynaTiji <leynatiji@icloud.com>
2026-03-19 16:15:08 +0000

    changed get id func names

--- tests/test_movies.py
+++ tests/test_movies.py
@@ -8,17 +8,18 @@ def test_get_movies():
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
-def test_get_movies_by_id():
+def test_get_movie_by_id_1():
     response = client.get("/movies/by-id?movie_id=1")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
-def test_get_movies_by_id():
+def test_get_movie_by_id_269():
     response = client.get("/movies/by-id?movie_id=269")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
-def test_get_movies_by_id():
+def test_get_movie_by_id_not_found():
     response = client.get("/movies/by-id?movie_id=2000000")
     assert response.status_code == 404
     assert response.json()["detail"] == "Movie not found"
+
