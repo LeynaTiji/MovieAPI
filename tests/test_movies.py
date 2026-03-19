e381c34a446734258dca1f89aca3ecb4a9ba10df
LeynaTiji <leynatiji@icloud.com>
2026-03-19 16:12:16 +0000

    added movie id test

--- tests/test_movies.py
+++ tests/test_movies.py
@@ -7,3 +7,18 @@ def test_get_movies():
     response = client.get("/movies")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
+
+def test_get_movies_by_id():
+    response = client.get("/movies/by-id?movie_id=1")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+def test_get_movies_by_id():
+    response = client.get("/movies/by-id?movie_id=269")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+def test_get_movies_by_id():
+    response = client.get("/movies/by-id?movie_id=2000000")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Movie not found"
