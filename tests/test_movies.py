62a38635d7876b30f65808033445a68268caf77c
LeynaTiji <leynatiji@icloud.com>
2026-03-19 16:21:43 +0000

    pytests for movie link endpoint

--- tests/test_movies.py
+++ tests/test_movies.py
@@ -23,3 +23,27 @@ def test_get_movie_by_id_not_found():
     assert response.status_code == 404
     assert response.json()["detail"] == "Movie not found"
 
+def test_get_movie_by_id_missing_param():
+    response = client.get("/movies/by-id")
+    assert response.status_code == 422
+
+def test_get_movie_by_link_oliver_twist():
+    response = client.get("/movies/by-link?movie_link=m/10005499-oliver_twist")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+def test_get_movie_by_link_eye():
+    response = client.get("/movies/by-link?movie_link=m/10008606-eye")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+def test_get_movie_404():
+    response = client.get("/movies/by-link?movie_link=m/20666666666-car")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Movie not found"
+
+def test_get_movie_by_link_missing_param():
+    response = client.get("/movies/by-link")
+    assert response.status_code == 422
+
+    
