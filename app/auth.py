5c480269bb2d66488b3839e8ab651d1c80ec62e6
LeynaTiji <leynatiji@icloud.com>
2026-03-20 02:35:00 +0000

    function that creates a token and updates expiry time of token

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,7 @@
 from jose import JWTError, jwt
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
 
 # code implemented with help from https://fastapi.tiangolo.com/tutorial/security/oauth2-jwt/#hash-and-verify-the-passwords
 
@@ -13,3 +14,13 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
+
+def verify_password(plain: str, hashed: str) -> bool:
+    return pwd_context.verify(plain, hashed)
+
+def create_token(data: dict):
+    data_to_encode = data.copy()
+    # calculate when token will expire based on moment its created
+    token_expiry = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    data_to_encode.update({"exp": token_expiry})
+    return jwt.encode(data_to_encode, SECRET_KEY, algorithm=ALGORITHM)
