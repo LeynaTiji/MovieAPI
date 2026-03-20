d61947b263f3bd7c92b85799115b7e2b1281ee71
LeynaTiji <leynatiji@icloud.com>
2026-03-20 02:46:43 +0000

    tken schema

--- app/schemas.py
+++ app/schemas.py
@@ -5,6 +5,10 @@ class UserCreate(BaseModel):
     username: str
     password: str
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
 class MovieBase(BaseModel):
     title: Optional[str] = None
     info: Optional[str] = None
