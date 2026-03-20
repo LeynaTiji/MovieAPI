d7a4deef744c12855ddddf63183929e5a0291e5d
LeynaTiji <146225194+LeynaTiji@users.noreply.github.com>
2026-03-20 19:58:35 +0000

    added no overriding

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(override=False)
 
 
 #db location
