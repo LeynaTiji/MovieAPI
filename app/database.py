1189bccc266d637cf941d61a1bf7a789fe249e33
LeynaTiji <146225194+LeynaTiji@users.noreply.github.com>
2026-03-20 15:17:45 +0000

    add postgresql

--- app/database.py
+++ app/database.py
@@ -4,6 +4,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 #db location
 DATABASE_URL = "sqlite:///./movies.db"
 
+#add postgress
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(bind=engine)
