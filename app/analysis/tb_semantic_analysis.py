05445bb06d7b63e1c25540e4cc9d1930feaaf7ab
LeynaTiji <leynatiji@icloud.com>
2026-03-20 14:34:52 +0000

    changed hf to textblob for semantics as hf just was not compatible for deployment

---
+++ app/analysis/tb_semantic_analysis.py
@@ -0,0 +1,21 @@
+from textblob import TextBlob
+
+def review_semantics(reviews: list[str]):
+
+    sentiment = []
+
+    for r in reviews:
+        # sentiment analysis
+        blob = TextBlob(r)
+        # polarity is between -1 and 1
+        sentiment.append(blob.sentiment.polarity)
+    
+    if not sentiment:
+        return "UNKNOWN", 0.0
+
+    average_score = sum(sentiment) / len(sentiment)
+
+    label = "POSITIVE" if average_score > 0 else "NEGATIVE"
+
+    return label, abs(average_score)
+
